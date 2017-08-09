9c9cb951b82724a2016959b4e587584c9788da79
marsqinht <qinhaitaocn@163.com>
2017-08-09 17:44:36 +0800

    self| reduce

--- test.js
+++ test.js
@@ -49,14 +49,22 @@ console.log(objList)
     let keys = !isArrayLike(list) && __.keys(list)
     let length = (keys || list).length
     let results = Array(length)
-
-    // console.log(keys, isArrayLike(list), length)
-
     for (let i = 0; i < length; i++) {
       let result = keys ? interatee(list[keys[i]], keys[i], list) : interatee(list[i], i, list)
       results[i] = result
     }
     return results
+  }
+
+  __.reduce = (list, interatee, memo = 0, context) => {
+    if (!isArrayLike(list)) return
+    let result
+    let length = list.length
+    for (let i = 0; i < length; i++) {
+      result = interatee(memo, list[i], i, list)
+      memo = result
+    }
+    return result
   }
 
   __.keys = (object) => {
@@ -70,7 +78,17 @@ console.log(objList)
     return arr
   }
 
-  console.log(__.map(objList, function (el, index) {
+  console.log(__.map(arr, function (el, index) {
     return el + 3
   }))
+
+  console.log(__.reduce(arr, function (memo, num, d, c) {
+    console.log(memo, num)
+    return memo + num
+  }, 3))
 }())
+
+// var sum = _.reduce([1, 2, 4, 5, 6, 7, 8], function (memo, num, d, c) {
+//   console.log(memo, num)
+//   return memo + num
+// }, 3)
